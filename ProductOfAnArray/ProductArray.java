20345a714f9dd85b421331a4cbe54a7636bbb190
ramesh-singh-nit <ramesh.singh.nit@gmail.com>
2026-05-23 17:55:31 -0700

    Added product of an array solution.

---
+++ ProductOfAnArray/ProductArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class ProductArray {
+    public int[] productExceptSelf(int[] nums) {
+        int [] result = new int[nums.length];
+        Arrays.fill(result, 1);
+
+        int pre= 1, post= 1;
+
+        for(int i=0; i<nums.length; i++){
+            result[i]= result[i] * pre;
+            pre= pre * nums[i];
+        }
+
+        for(int i= nums.length-1; i>=0; i--){
+            result[i] = result[i] * post;
+            post= post * nums[i];
+        }
+
+
+        return result;
+
+    }
+}
