23013b0de9a88df0fe5b47ddacf7894ee6fbd0a6
ramesh-singh-nit <ramesh.singh.nit@gmail.com>
2026-05-24 10:47:54 -0700

    Solution for valid sudoku.

---
+++ ValidSudoku/ValidSudoku.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        int N= 9;
+
+        Set<Character> [] rows = new HashSet[N];
+        Set<Character> [] cols = new HashSet[N];
+        Set<Character> [] boxes = new HashSet[N];
+
+        for(int i=0; i<N; i++){
+            rows[i]= new HashSet<>();
+            cols[i]= new HashSet<>();
+            boxes[i]= new HashSet<>();
+        }
+
+        for(int r= 0; r< N; r++){
+            for(int c =0; c< N; c++){
+                char ch= board[r][c];
+
+                if(ch == '.'){
+                    continue;
+                }
+
+                if(!rows[r].add(ch)){
+                    return false;
+                }
+                if(!cols[r].add(ch)){
+                    return false;
+                }
+                int index= (r/3) * 3 + c/3;
+
+                if(!boxes[index].add(ch)){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
