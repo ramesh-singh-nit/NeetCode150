ab375b063f400c8ce1ec09c5b1f057645882870c
ramesh-singh-nit <ramesh.singh.nit@gmail.com>
2026-05-28 21:51:16 -0700

    Added solution for reversing LinkedList.

---
+++ ValidParanthesis/ValidParanthesis.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParanthesis {
+    public boolean isValid(String s) {
+        Map<Character, Character> charMap= new HashMap<>();
+
+        charMap.put(')', '(');
+        charMap.put('}', '{');
+        charMap.put(']', '[');
+        Stack<Character> charStack= new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch == '{' || ch== '[' || ch== '('){
+                charStack.push(ch);
+            }
+
+            if(charMap.containsKey(ch)){
+                if(!charStack.isEmpty() && charStack.peek()==
+                        charMap.get(ch)){
+                    charStack.pop();
+                }else{
+                    return false;
+                }
+
+            }
+        }
+        return charStack.isEmpty();
+    }
+}
